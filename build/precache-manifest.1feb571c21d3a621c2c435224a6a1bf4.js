14dd30855d241f40863e9c84ad4a6c6d58af926e
Emil Grabowski <43807341+Emil9@users.noreply.github.com>
2020-05-08 08:45:42 +0200

    Add files via upload

---
+++ build/precache-manifest.1feb571c21d3a621c2c435224a6a1bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e497d66fe01415ec8407a0ec307fdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e188c23c6345e56bd906",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "fdb425e809d50e0287ae",
+    "url": "/static/js/2.b8e25800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8e25800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e188c23c6345e56bd906",
+    "url": "/static/js/main.0d8ac466.chunk.js"
+  },
+  {
+    "revision": "0f75070fe5a200d3072a",
+    "url": "/static/js/runtime-main.c47da6bb.js"
+  }
+]);
